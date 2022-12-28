a5c54c1e52c2bac562f33ffebca8e29cef8e94e5
Michael Mogessie <7068628+michaelmogessie@users.noreply.github.com>
2022-12-28 16:23:11 -0500

    Added more tests

--- tests/testthat/test-alpha_correction.R
+++ tests/testthat/test-alpha_correction.R
@@ -29,3 +29,11 @@ test_that("it should calculate alphas 1", {
 test_that("it should calculate alphas 2", {
   expect_equal(lapply(get_alphas(list(0.01,0.08,0.039), .07), round, 5), lapply(list(0.02333333,0.04666667,0.07), round, 5))
 })
+
+test_that("it should calculate alphas 3", {
+  expect_equal(lapply(get_alphas(list(0.08,0.01,0.039)), round, 5), lapply(list(0.01666667,0.03333333,0.05), round, 5))
+})
+
+test_that("it should calculate alphas 4", {
+  expect_equal(lapply(get_alphas(list(0.039,0.08,0.01), .07), round, 5), lapply(list(0.02333333,0.04666667,0.07), round, 5))
+})
